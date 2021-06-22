0baecca8143d879e874dd5bcdaa1349941e18591
Cliff Chipman <61033936+cchipman21804@users.noreply.github.com>
2021-06-22 16:28:42 -0400

    Add files via upload

--- metarEnthalpyEconomizer_Evaluator.py
+++ metarEnthalpyEconomizer_Evaluator.py
@@ -230,28 +230,6 @@ outdoorDewPointKey = str(round(dewPoint))
 rhOutdoor = float(dbTempSatTable[outdoorDewPointKey])/float(dbTempSatTable[outdoorDBTempKey])
 print(f"Relative Humidity: {round(rhOutdoor*100,1)}%")
 #
-# Wind Direction:
-if "AUTO" in wxdata:
-    startIdx = 38
-    endIdx = 41
-else:
-    startIdx = 33
-    endIdx = 36
-#
-windDir = wxdata[startIdx:endIdx]
-#
-if windDir == 'VRB':
-    print("\n *** Wind is too variable --- ineffective for economizer operation...\n")
-    exit(0)
-#
-# Convert wind direction to float for energy calculations
-try:
-    windDir = float(windDir)
-except:
-    print("\n *** FATAL ERROR: Invalid weather data [{windDir}] ***")
-    print(" *** Returning to operating system with errorlevel 2.")
-    exit(2)
-#
 # Wind Speed:
 if "AUTO" in wxdata:
     startIdx = 41
@@ -279,6 +257,29 @@ except:
 # Convert wind speed to MPH for display
 windSpeedMph = round(windSpeedKnots / 0.868976)
 #
+# Wind Direction:
+if "AUTO" in wxdata:
+    startIdx = 38
+    endIdx = 41
+else:
+    startIdx = 33
+    endIdx = 36
+#
+windDir = wxdata[startIdx:endIdx]
+#
+if windDir == 'VRB':
+    print("\n *** Wind is too variable --- ineffective for economizer operation...\n")
+#    exit(0)
+#
+# Convert wind direction to float for energy calculations
+else:
+    try:
+        windDir = float(windDir)
+    except:
+        print("\n *** FATAL ERROR: Invalid weather data [{windDir}] ***")
+        print(" *** Returning to operating system with errorlevel 2.")
+        exit(2)
+#
 print(f"Wind: {windDir} degrees @{windSpeedKnots} Knots ({windSpeedMph} MPH)")
 while not gust == '':
     try:
@@ -304,127 +305,131 @@ print(f"\n *** Outdoor Enthalpy: {round(OutdoorEnthalpy,1)} BTU per lb of dry ai
 #
 if windSpeedKnots == 0:
     print("\n *** Wind is calm --- ineffective for economizer operation...\n")
-    exit(0)
+#    exit(0)
+#
+if windDir == 'VRB' or windSpeedKnots == 0:
+    exit(0) # Exit to operating system now - indoor conditions not needed
+else:
 #
 # Calculate indoor humidity ratio from indoor relative humidity and dry bulb temperature saturation lookup table
 #
 # Data entry of indoor conditions
-validInput = False
-while validInput == False:
-    t = input("Enter indoor dry bulb temperature (0-120 deg F): ")
-    try:
-        indoorDBTemp = round(float(t))
-        if indoorDBTemp >= 0 & indoorDBTemp <= 120:
-            validInput = True
-        else:
-            print("\n *** Temperature must be between 0 & 120 degrees (inclusive). ***\n")
-    except ValueError:
-        print("\n *** Temperature must be a number within the specified limits. ***\n")
+    validInput = False
+    while validInput == False:
+        t = input("Enter indoor dry bulb temperature (0-120 deg F): ")
+        try:
+            indoorDBTemp = round(float(t))
+            if indoorDBTemp >= 0 & indoorDBTemp <= 120:
+                validInput = True
+            else:
+                print("\n *** Temperature must be between 0 & 120 degrees (inclusive). ***\n")
+        except ValueError:
+            print("\n *** Temperature must be a number within the specified limits. ***\n")
 #
 # Uncomment to debug:
 # print(f"Saturated Specific Humidity: {dbTempSatTable[t]} gr/LB")
 #
-validInput = False
-while validInput == False:
-    rh = input("Enter indoor relative humidity (0-100 %): ")
-    try:
-        rhIndoor = round(float(rh))
-        if rhIndoor >= 0 & rhIndoor <= 100:
-            validInput = True
-        else:
-            print("\n *** Relative Humidity must be between 0 & 100 (inclusive). ***\n")
-    except ValueError:
-        print("\n *** Relative Humidity must be a number within the specified limits. ***\n")
+    validInput = False
+    while validInput == False:
+        rh = input("Enter indoor relative humidity (0-100 %): ")
+        try:
+            rhIndoor = round(float(rh))
+            if rhIndoor >= 0 & rhIndoor <= 100:
+                validInput = True
+            else:
+                print("\n *** Relative Humidity must be between 0 & 100 (inclusive). ***\n")
+        except ValueError:
+            print("\n *** Relative Humidity must be a number within the specified limits. ***\n")
 #
 # Convert relative humidity percentage to decimal
-rhIndoor = rhIndoor/100
+    rhIndoor = rhIndoor/100
 #
 # x = input("Enter indoor humidity ratio")
 #
 # Look up saturation specific humidity of indoor dry bulb temperature in dictionary
 # Multiply the saturation specific humidity by the relative humidity to obtain the specific humidity
-x = float(dbTempSatTable[str(indoorDBTemp)]) * rhIndoor
+    x = float(dbTempSatTable[str(indoorDBTemp)]) * rhIndoor
 #
 # convert from gr/LB to LB/LB for enthalpy formula
-x = x/7000
+    x = x/7000
 #
-IndoorEnthalpy = (0.240 * indoorDBTemp) + x * (0.444 * indoorDBTemp + 1061)
-print(f"\n *** Indoor Enthalpy: {round(IndoorEnthalpy,1)} BTU per lb of dry air")
-print("\n")
+    IndoorEnthalpy = (0.240 * indoorDBTemp) + x * (0.444 * indoorDBTemp + 1061)
+    print(f"\n *** Indoor Enthalpy: {round(IndoorEnthalpy,1)} BTU per lb of dry air")
+    print("\n")
 #
 # Obtain window opening dimensions in inches
-validInput = False
-while validInput == False:
-    windowWidthInches = input("Enter window opening width in inches (20-60): ")
-    try:
-        windowWidthInches = float(windowWidthInches)
-        if windowWidthInches < 20:
-            print("\n *** Window width must be greater than or equal to 20 inches. ***\n")
-        elif windowWidthInches > 60:
-            print("\n *** Window width must be less than or equal to 60 inches. ***\n")
-        else:
-            windowWidthFeet = windowWidthInches / 12 # convert inches to feet
-            validInput = True
-    except:
-        print("Window width must be a number within the specified limits. ***")
+    validInput = False
+    while validInput == False:
+        windowWidthInches = input("Enter window opening width in inches (20-60): ")
+        try:
+            windowWidthInches = float(windowWidthInches)
+            if windowWidthInches < 20:
+                print("\n *** Window width must be greater than or equal to 20 inches. ***\n")
+            elif windowWidthInches > 60:
+                print("\n *** Window width must be less than or equal to 60 inches. ***\n")
+            else:
+                windowWidthFeet = windowWidthInches / 12 # convert inches to feet
+                validInput = True
+        except:
+            print("Window width must be a number within the specified limits. ***")
 #
-validInput = False
-while validInput == False:
-    windowHeightInches = input("Enter window opening height in inches (1-30): ")
-    try:
-        windowHeightInches = float(windowHeightInches)
-        if windowHeightInches < 1:
-            print("\n *** Window height must be greater than or equal to 1 inch. ***\n")
-        elif windowHeightInches > 30:
-            print("\n *** Window height must be less than or equal to 30 inches. ***\n")
-        else:
-            windowHeightFeet = windowHeightInches / 12 # convert inches to feet
-            validInput = True
-    except:
-        print("\n *** Window height must be a number within the specified limits. ***")
+    validInput = False
+    while validInput == False:
+        windowHeightInches = input("Enter window opening height in inches (1-30): ")
+        try:
+            windowHeightInches = float(windowHeightInches)
+            if windowHeightInches < 1:
+                print("\n *** Window height must be greater than or equal to 1 inch. ***\n")
+            elif windowHeightInches > 30:
+                print("\n *** Window height must be less than or equal to 30 inches. ***\n")
+            else:
+                windowHeightFeet = windowHeightInches / 12 # convert inches to feet
+                validInput = True
+        except:
+            print("\n *** Window height must be a number within the specified limits. ***")
 #
 # Obtain quantity of window openings
-validInput = False
-while validInput == False:
-    windowQuantity = input("Enter quantity of window openings (1-9): ")
-    try:
-        windowQuantity = int(windowQuantity)
-        if windowQuantity < 1 or windowQuantity > 9:
+    validInput = False
+    while validInput == False:
+        windowQuantity = input("Enter quantity of window openings (1-9): ")
+        try:
+            windowQuantity = int(windowQuantity)
+            if windowQuantity < 1 or windowQuantity > 9:
+                print("\n")
+            else:
+                validInput = True
+        except:
             print("\n")
-        else:
-            validInput = True
-    except:
-        print("\n")
 #
 # Obtain window opening area in sqft
-windowOpeningArea = windowWidthFeet * windowHeightFeet * windowQuantity
+    windowOpeningArea = windowWidthFeet * windowHeightFeet * windowQuantity
 #
 # Obtain window facing direction in compass degrees
-validInput = False
-while validInput == False:
-    windowOpeningDir = input("Enter compass direction of window openings (in degrees from True North) (0 - 359): ")
-    try:
-        windowOpeningDir = float(windowOpeningDir)
-        if windowOpeningDir < 0 or windowOpeningDir > 359:
+    validInput = False
+    while validInput == False:
+        windowOpeningDir = input("Enter compass direction of window openings (in degrees from True North) (0 - 359): ")
+        try:
+            windowOpeningDir = float(windowOpeningDir)
+            if windowOpeningDir < 0 or windowOpeningDir > 359:
+                print("\n")
+            else:
+                validInput = True
+        except:
             print("\n")
-        else:
-            validInput = True
-    except:
-        print("\n")
 #
 # Obtain air flow through window (in CFM) from outdoor wind speed & direction
 # relative to window opening direction & dimensions
 #
-windDirOffset = abs(windDir - windowOpeningDir)
+    windDirOffset = abs(windDir - windowOpeningDir)
 #
-if windDirOffset >= 90:
-      massFlowRate = 0 # airMassFlowRate = 0
-      print("\n *** The wind is blowing from an ineffective direction. ***")
-else:
-      massFlowRate = windSpeedFpm                                # massFlowRate = windSpeed in fpm
-      massFlowRate = massFlowRate * abs(math.cos(windDirOffset)) # compensate for wind direction relative to window opening direction
-      massFlowRate = massFlowRate * windowOpeningArea            # convert fpm to CFM based on size of window opening (in sqft)
-      massFlowRate = massFlowRate / 4.5                          # Convert CFM to lb/hr
+    if windDirOffset >= 90:
+          massFlowRate = 0 # airMassFlowRate = 0
+          print("\n *** The wind is blowing from an ineffective direction. ***")
+    else:
+          massFlowRate = windSpeedFpm                                # massFlowRate = windSpeed in fpm
+          massFlowRate = massFlowRate * abs(math.cos(windDirOffset)) # compensate for wind direction relative to window opening direction
+          massFlowRate = massFlowRate * windowOpeningArea            # convert fpm to CFM based on size of window opening (in sqft)
+          massFlowRate = massFlowRate / 4.5                          # Convert CFM to lb/hr
 #
 # Compare indoor enthalpy to outdoor enthalpy and determine if economizer should be used
 #
@@ -441,19 +446,19 @@ else:
 # (h2-h1) = change in enthalpy of substance in BTU/lb
 #
 # Calculate heat flow rate in BTU/hr
-Q = massFlowRate * (IndoorEnthalpy - OutdoorEnthalpy)
-if Q < 0:
-    mode = "heating"
-    equiv = Q / 3412.14163  # convert to KW
-    unit = "KW"
-else:
-    mode = "cooling"
-    equiv = Q / 12000  # convert to tons
-    unit = "tons"
+    Q = massFlowRate * (IndoorEnthalpy - OutdoorEnthalpy)
+    if Q < 0:
+        mode = "heating"
+        equiv = Q / 3412.14163  # convert to KW
+        unit = "KW"
+    else:
+        mode = "cooling"
+        equiv = Q / 12000  # convert to tons
+        unit = "tons"
 #
 # NOTE: Negative values of Q are heating BTUs/hr
-print(f"\nOpening the windows will provide {abs(round(Q,2))} BTU/hr of {mode}.")
-print(f"Equivalent to {abs(round(equiv,2))} {unit}.\n")
+    print(f"\nOpening the windows will provide {abs(round(Q,2))} BTU/hr of {mode}.")
+    print(f"Equivalent to {abs(round(equiv,2))} {unit}.\n")
 #
 print("\n *** Returning to operating system...\n")
 exit(0)
